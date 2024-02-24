f4c92d569c1c47002e9c74c00a8325f1d40859ee
yejj710 <jiajun227@zju.edu.cn>
2024-02-24 21:51:42 +0800

    code refactoring

---
+++ chapter3/exp1/minist/test.py
@@ -0,0 +1,67 @@
+import sys
+
+import secretflow as sf
+
+sys.path.append("/home/yejj/GradExperiments/chapter3_exp1")
+from utils.model import create_model
+
+from secretflow.ml.nn import FLModel
+from secretflow.security.aggregation import PlainAggregator
+
+sf.shutdown()
+#init 10 nodes and 1 server
+# nodes = [f"party{i}" for i in range(10)]
+# nodes.append('server')
+
+# init nodes
+# sf.init(nodes, address="local")
+sf.init(['alice', 'bob','server'], address="local")
+# init PYU device
+# devices = {}
+# for i in range(10):
+#     devices[i] = sf.PYU(f"party{i}")
+# agg_server = sf.PYU("server")
+alice, bob, agg_server = sf.PYU("alice"), sf.PYU("bob"), sf.PYU("server")
+
+
+from secretflow.utils.simulation.datasets import load_mnist
+
+(x_train, y_train), (x_test, y_test) = load_mnist(parts=[alice, bob], normalized_x=True, categorical_y=True)
+print(x_train.partition_shape())
+
+num_classes = 10
+input_shape = (28, 28, 1)
+model = create_model(input_shape, num_classes, "lenet")
+
+plain_aggregator = PlainAggregator(agg_server)
+
+fed_plain_model = FLModel(server=agg_server,
+                    device_list=[alice, bob],
+                    model=model,
+                    aggregator=plain_aggregator,
+                    strategy="fed_avg_w",
+                    backend = "tensorflow")
+
+from secretflow.ml.nn.callbacks.early_stopping import EarlyStoppingEpoch
+
+earlystop_callback = EarlyStoppingEpoch(
+		monitor='val_loss', min_delta=0.005, mode='min',
+        patience=1)
+
+import time
+
+start_time = time.time()
+history = fed_plain_model.fit(x_train,
+                    y_train,
+                    validation_data=(x_test, y_test),
+                    epochs=1,
+                    sampler_method="batch",
+                    batch_size=128,
+                    aggregate_freq=1,
+                    callbacks=earlystop_callback)
+plain_time = time.time() - start_time
+# print("cost time : ", time.time()-start_time)
+print(history)
+
+pred_y = fed_plain_model.predict(x_test, batch_size=128)
+print(pred_y)
---
+++ chapter3/exp3_exp4/test/test_f1.py
@@ -0,0 +1,47 @@
+import sys
+import secretflow as sf
+sys.path.append("/home/yejj/GradExperiments/chapter3_exp1")
+from utils.model import create_model
+from secretflow.ml.nn import FLModel
+from secretflow.security.aggregation import PlainAggregator
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from copy_ndarray import create_ndarray
+from secretflow.device import reveal
+
+
+if __name__ == "__main__":
+    sf.shutdown()
+    # sf.init(nodes, address="local")
+    sf.init(['a', 'b'], address="local")
+    # init PYU device
+    alice, bob = sf.PYU("a"), sf.PYU("b")
+
+    filepath = "/home/yejj/.secretflow/datasets/mnist.npz"
+    with np.load(filepath) as f:
+        yt = f['y_test']
+    print(yt[0:10], yt[5000:5015])
+    encoder = OneHotEncoder(sparse=False)
+    yt = encoder.fit_transform(yt.reshape(-1, 1))
+    # print(yt[0:5])
+    h_arr = create_ndarray(yt, parts=[alice, bob], axis=0)
+    # print(h_arr)
+    result = np.array([])
+    print("result", result)
+    for device in [alice, bob]:
+        temp = h_arr.partitions[device]
+        # print(temp)
+        y_pred = np.argmax(np.array(reveal(temp)[0:5]), axis=1)
+        result = np.concatenate((result, y_pred))
+        # print(type(y_pred))
+        # print(y_pred)
+        # print(reveal(temp)[0:5])
+        # [7 2 1 0 4]  [3 9 9 8 4]
+    print(result, type(result))
+    from sklearn.metrics import precision_score, recall_score, f1_score
+    y = [7,2,1,0,4,3,9,9,8,4]
+    f1_micro = f1_score(y, result, average='micro')
+    print(f1_micro)
+
+    
+
